fb9b826dad19b55e1efee851eb605fba72f3c060
obrienu <obrienules@gmail.com>
2020-12-29 14:37:21 +0100

    Adds Office TransferDto

---
+++ DTOs/TransferDTOs/OfficeTransferDTO.cs
@@ -0,0 +1,17 @@
+using HaloBiz.Model;
+
+namespace HaloBiz.DTOs.TransferDTOs
+{
+    public class OfficeTransferDTO
+    {
+        public long Id { get; set; }        
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public StateWithoutLGATransferDto State { get; set; }
+        public LGATransferDTO LGA { get; set; }
+        public string Street { get; set; }
+        public string PhoneNumber { get; set; }
+        public UserProfileTransferDTO Head { get; set; }
+
+    }
+}
