02400106162c765933c0c8e93c2dac43e3b5c6c6
Jayora50 <umorenjoshua50@gmail.com>
2021-01-12 20:29:48 +0100

    Minor change

--- Repository/LAMS/IFinancialVoucherTypeRepository.cs
+++ Repository/LAMS/IFinancialVoucherTypeRepository.cs
@@ -6,10 +6,10 @@ namespace HaloBiz.Repository.LAMS
 {
     public interface IFinancialVoucherTypeRepository
     {
-         Task<FinanceVoucherType> SaveFinanceVoucherType(FinanceVoucherType leadOrigin);
+         Task<FinanceVoucherType> SaveFinanceVoucherType(FinanceVoucherType voucherType);
          Task<FinanceVoucherType> FindFinanceVoucherTypeById(long Id);
-        Task<IEnumerable<FinanceVoucherType>> FindAllFinanceVoucherTypes();
-         Task<FinanceVoucherType> UpdateFinanceVoucherType(FinanceVoucherType leadOrigin);
-         Task<bool> DeleteFinanceVoucherType(FinanceVoucherType leadOrigin);
+         Task<IEnumerable<FinanceVoucherType>> FindAllFinanceVoucherTypes();
+         Task<FinanceVoucherType> UpdateFinanceVoucherType(FinanceVoucherType voucherType);
+         Task<bool> DeleteFinanceVoucherType(FinanceVoucherType voucherType);
     }
 }
