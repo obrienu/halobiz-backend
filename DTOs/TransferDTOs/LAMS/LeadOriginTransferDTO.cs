4504ec1b3f897f299640d4ed59dcb88b3ea5950e
obrienu <obrienules@gmail.com>
2021-01-12 07:34:39 +0100

    Removes LeadTypeId field from DTO

--- DTOs/TransferDTOs/LAMS/LeadOriginTransferDTO.cs
+++ DTOs/TransferDTOs/LAMS/LeadOriginTransferDTO.cs
@@ -6,11 +6,7 @@ namespace HaloBiz.DTOs.TransferDTOs.LAMS
     {
         [Key]
         public long Id { get; set; }
-        [Required]
         public string Caption { get; set; }
-        [StringLength(1000)]
         public string Description { get; set; }
-        [Required]
-        public long LeadTypeId { get; set; }
     }
 }
