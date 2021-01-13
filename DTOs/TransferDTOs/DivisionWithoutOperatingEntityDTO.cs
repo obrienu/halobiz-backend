c1868013448c4c4d0b1ae64014a0a451412db133
obrienu <obrienules@gmail.com>
2021-01-13 11:46:21 +0100

    Adds DivisionWithouthOperatingEntity DTO and its mapping

---
+++ DTOs/TransferDTOs/DivisionWithoutOperatingEntityDTO.cs
@@ -0,0 +1,10 @@
+namespace HaloBiz.DTOs.TransferDTOs
+{
+    public class DivisionWithoutOperatingEntityDTO
+    {
+         public long Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string MissionStatement { get; set; }
+    }
+}
