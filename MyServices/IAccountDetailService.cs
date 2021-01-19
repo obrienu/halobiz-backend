4bcf48249317ba761224c6f592f0e626fa7fbfa0
Umoren Joshua <umorenjoshua50@gmail.com>
2021-01-19 23:25:45 +0100

    Added service and implementation for account master and account details and fixed bug with accounts and accountclass

---
+++ MyServices/IAccountDetailService.cs
@@ -0,0 +1,21 @@
+﻿using HaloBiz.DTOs.ApiDTOs;
+using HaloBiz.DTOs.ReceivingDTOs;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HaloBiz.MyServices
+{
+    public interface IAccountDetailService
+    {
+        Task<ApiResponse> AddAccountDetail(HttpContext context, AccountDetailReceivingDTO accountDetailReceivingDTO);
+        Task<ApiResponse> GetAccountDetailById(long id);
+        Task<ApiResponse> GetAccountDetailByAlias(long alias);
+        Task<ApiResponse> GetAccountDetailByName(string name);
+        Task<ApiResponse> GetAllAccountDetails();
+        Task<ApiResponse> UpdateAccountDetail(long id, AccountDetailReceivingDTO accountDetailReceivingDTO);
+        Task<ApiResponse> DeleteAccountDetail(long id);
+    }
+}
---
+++ MyServices/IAccountMasterService.cs
@@ -0,0 +1,21 @@
+﻿using HaloBiz.DTOs.ApiDTOs;
+using HaloBiz.DTOs.ReceivingDTOs;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HaloBiz.MyServices
+{
+    public interface IAccountMasterService
+    {
+        Task<ApiResponse> AddAccountMaster(HttpContext context, AccountMasterReceivingDTO accountMasterReceivingDTO);
+        Task<ApiResponse> GetAccountMasterById(long id);
+        Task<ApiResponse> GetAccountMasterByAlias(long alias);
+        Task<ApiResponse> GetAccountMasterByName(string name);
+        Task<ApiResponse> GetAllAccountMasters();
+        Task<ApiResponse> UpdateAccountMaster(long id, AccountMasterReceivingDTO accountMasterReceivingDTO);
+        Task<ApiResponse> DeleteAccountMaster(long id);
+    }
+}
