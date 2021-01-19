483a1a5d6065cb0e8a0028ecc03b36cd0de5323e
Umoren Joshua <umorenjoshua50@gmail.com>
2021-01-19 23:24:15 +0100

    Added Repositories for Account Details and Account Master

---
+++ Repository/IAccountMasterRepository.cs
@@ -0,0 +1,26 @@
+﻿using HaloBiz.Model.AccountsModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HaloBiz.Repository
+{
+    public interface IAccountMasterRepository
+    {
+        Task<AccountMaster> SaveAccountMaster(AccountMaster accountMaster);
+
+        Task<AccountMaster> FindAccountMasterById(long Id);
+
+        Task<AccountMaster> FindAccountMasterByAlias(long alias);
+
+        Task<AccountMaster> FindAccountMasterByName(string name);
+
+
+        Task<IEnumerable<AccountMaster>> FindAllAccountMasters();
+
+        Task<AccountMaster> UpdateAccountMaster(AccountMaster accountMaster);
+
+        Task<bool> DeleteAccountMaster(AccountMaster accountMaster);
+    }
+}
---
+++ Repository/Impl/AccountMasterRepositoryImpl.cs
@@ -0,0 +1,84 @@
+﻿using HaloBiz.Data;
+using HaloBiz.Model.AccountsModel;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HaloBiz.Repository.Impl
+{
+    public class AccountMasterRepositoryImpl : IAccountMasterRepository
+    {
+        private readonly DataContext _context;
+        private readonly ILogger<AccountMasterRepositoryImpl> _logger;
+        public AccountMasterRepositoryImpl(DataContext context, ILogger<AccountMasterRepositoryImpl> logger)
+        {
+            this._logger = logger;
+            this._context = context;
+
+        }
+
+        public async Task<bool> DeleteAccountMaster(AccountMaster accountMaster)
+        {
+            accountMaster.IsDeleted = true;
+            _context.AccountMasters.Update(accountMaster);
+            return await SaveChanges();
+        }
+
+        public async Task<AccountMaster> FindAccountMasterByAlias(long alias)
+        {
+            return await _context.AccountMasters.FirstOrDefaultAsync(accountMaster => accountMaster.AccountMasterAlias == alias && accountMaster.IsDeleted == false);
+
+        }
+
+        public async Task<AccountMaster> FindAccountMasterByName(string name)
+        {
+            return await _context.AccountMasters.FirstOrDefaultAsync(accountMaster => accountMaster.Name == name && accountMaster.IsDeleted == false);
+
+        }
+
+        public async Task<AccountMaster> FindAccountMasterById(long Id)
+        {
+            return await _context.AccountMasters.FirstOrDefaultAsync(accountMaster => accountMaster.Id == Id && accountMaster.IsDeleted == false);
+        }
+
+        public async Task<IEnumerable<AccountMaster>> FindAllAccountMasters()
+        {
+            return await _context.AccountMasters.Where(user => user.IsDeleted == false).ToListAsync();
+
+        }
+        public async Task<AccountMaster> SaveAccountMaster(AccountMaster accountMaster)
+        {
+            var AccountMasterEntity = await _context.AccountMasters.AddAsync(accountMaster);
+            if (await SaveChanges())
+            {
+                return AccountMasterEntity.Entity;
+            }
+            return null;
+        }
+
+        public async Task<AccountMaster> UpdateAccountMaster(AccountMaster accountMaster)
+        {
+            var AccountMasterEntity = _context.AccountMasters.Update(accountMaster);
+            if (await SaveChanges())
+            {
+                return AccountMasterEntity.Entity;
+            }
+            return null;
+        }
+        private async Task<bool> SaveChanges()
+        {
+            try
+            {
+                return await _context.SaveChangesAsync() > 0;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                return false;
+            }
+        }
+    }
+}
