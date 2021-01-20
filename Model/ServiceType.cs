3c974fa205ca59ac39de9c28f4ff7502e78bc4d6
obrienu <obrienules@gmail.com>
2021-01-20 11:04:12 +0100

    Adds relationship to services

--- Model/ServiceType.cs
+++ Model/ServiceType.cs
@@ -1,7 +1,9 @@
+using System.Collections.Generic;
+
 namespace HaloBiz.Model
 {
     public class ServiceType : SetupBaseModel
     {
-        
+        public IEnumerable<Services> Services { get; set; }
     }
 }
