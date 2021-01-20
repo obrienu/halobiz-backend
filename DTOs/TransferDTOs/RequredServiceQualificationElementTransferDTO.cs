b48d7ea5d87f5b5035ad4ba8291c452290933efe
Umoren Joshua <umorenjoshua50@gmail.com>
2021-01-20 15:21:35 +0100

    Adds DTOs for RequredServiceQualificationElement

---
+++ DTOs/TransferDTOs/RequredServiceQualificationElementTransferDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HaloBiz.DTOs.TransferDTOs
+{
+    public class RequredServiceQualificationElementTransferDTO : BaseSetupTransferDTO
+    {
+        public ServiceCategoryTransferDTO ServiceCategory { get; set; }
+    }
+}
