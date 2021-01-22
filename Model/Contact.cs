65c20ef86fd47c5911ef8abdddc3bea63e597302
Umoren Joshua <umorenjoshua50@gmail.com>
2021-01-22 14:00:52 +0100

    Adds Models

---
+++ Model/Contact.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HaloBiz.Model
+{
+    public class Contact
+    {
+        [Key]
+        public long Id { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required, RegularExpression("\\d{10,15}")]
+        public string MobileNumber { get; set; }
+        [Required]
+        public string Designation { get; set; }
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+        [Required]
+        public long CreatedById { get; set; }
+        public virtual UserProfile CreatedBy { get; set; }
+        public bool IsDeleted { get; set; } = false;
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public DateTime CreatedAt { get; set; }
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime UpdatedAt { get; set; }
+
+    }
+}
