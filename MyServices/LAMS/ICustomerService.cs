a205ec49247618ea2eb7eaa5feeeadafd01a0a92
Umoren Joshua <umorenjoshua50@gmail.com>
2021-01-24 20:46:48 +0100

    Added Customer Service

---
+++ MyServices/LAMS/ICustomerService.cs
@@ -0,0 +1,21 @@
+﻿using HaloBiz.DTOs.ApiDTOs;
+using HaloBiz.DTOs.ReceivingDTOs;
+using HaloBiz.DTOs.ReceivingDTOs.LAMS;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HaloBiz.MyServices.LAMS
+{
+    public interface ICustomerService
+    {
+        Task<ApiResponse> AddCustomer(HttpContext context, CustomerReceivingDTO customerReceivingDTO);
+        Task<ApiResponse> GetAllCustomers();
+        Task<ApiResponse> GetCustomerById(long id);
+        Task<ApiResponse> GetCustomerByName(string name);
+        Task<ApiResponse> UpdateCustomer(HttpContext context, long id, CustomerReceivingDTO customerReceivingDTO);
+        Task<ApiResponse> DeleteCustomer(long id);
+    }
+}
