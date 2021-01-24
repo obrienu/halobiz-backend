cf1a3c8e02e90053f41d7f74843025e768a5fa2a
obrienu <obrienules@gmail.com>
2021-01-24 12:12:22 +0100

    Adds update publish status apis to Services

--- MyServices/IServicesService.cs
+++ MyServices/IServicesService.cs
@@ -1,6 +1,7 @@
 using System.Threading.Tasks;
 using HaloBiz.DTOs.ApiDTOs;
 using HaloBiz.DTOs.ReceivingDTOs;
+using Microsoft.AspNetCore.Http;
 
 namespace HaloBiz.MyServices
 {
@@ -10,7 +11,12 @@ namespace HaloBiz.MyServices
         Task<ApiResponse> GetAllServices();
         Task<ApiResponse> GetServiceById(long id);
         Task<ApiResponse> GetServiceByName(string name);
-        Task<ApiResponse> UpdateServices(long id, ServicesReceivingDTO serviceReceivingDTO);
+        Task<ApiResponse> UpdateServices(HttpContext context, long id, ServicesReceivingDTO serviceReceivingDTO);
+        Task<ApiResponse> ApproveService(HttpContext context, long id);
+        Task<ApiResponse> DisapproveService(HttpContext context, long id);
+        Task<ApiResponse> RequestPublishService(HttpContext context, long id);
+
         Task<ApiResponse> DeleteService(long id);
+        Task<ApiResponse> DeleteService(HttpContext context, long id);
     }
 }
--- MyServices/Impl/ServicesServiceImpl.cs
+++ MyServices/Impl/ServicesServiceImpl.cs
@@ -4,9 +4,11 @@ using AutoMapper;
 using HaloBiz.DTOs.ApiDTOs;
 using HaloBiz.DTOs.ReceivingDTOs;
 using HaloBiz.DTOs.TransferDTOs;
+using HaloBiz.Helpers;
 using HaloBiz.Model;
 using HaloBiz.Model.ManyToManyRelationship;
 using HaloBiz.Repository;
+using Microsoft.AspNetCore.Http;
 
 namespace HaloBiz.MyServices.Impl
 {
@@ -15,11 +17,15 @@ namespace HaloBiz.MyServices.Impl
         private readonly IServicesRepository _servicesRepository;
         private readonly IMapper _mapper;
         private readonly IServiceRequiredServiceDocumentRepository _requiredServiceDocRepo;
+        private readonly IDeleteLogRepository _deleteLogRepo;
+        private readonly IModificationHistoryRepository _modificationRepo;
 
-        public ServicesServiceImpl(IServicesRepository servicesRepository, IMapper mapper, IServiceRequiredServiceDocumentRepository requiredServiceDocRepo)
+        public ServicesServiceImpl(IServicesRepository servicesRepository, IMapper mapper, IServiceRequiredServiceDocumentRepository requiredServiceDocRepo, IDeleteLogRepository deleteLogRepo, IModificationHistoryRepository modificationRepo)
         {
             this._mapper = mapper;
             this._requiredServiceDocRepo = requiredServiceDocRepo;
+            this._deleteLogRepo = deleteLogRepo;
+            this._modificationRepo = modificationRepo;
             this._servicesRepository = servicesRepository;
         }
 
@@ -86,13 +92,16 @@ namespace HaloBiz.MyServices.Impl
             return new ApiOkResponse(serviceTransferDTO);
         }
 
-        public async Task<ApiResponse> UpdateServices(long id, ServicesReceivingDTO serviceReceivingDTO)
+        public async Task<ApiResponse> UpdateServices(HttpContext context, long id, ServicesReceivingDTO serviceReceivingDTO)
         {
             var serviceToUpdate = await _servicesRepository.FindServicesById(id);
             if (serviceToUpdate == null)
             {
                 return new ApiResponse(404);
             }
+            var summary = $"Initial details before change, \n {serviceToUpdate.ToString()} \n";
+
+            
             serviceToUpdate.Name = serviceReceivingDTO.Name;
             serviceToUpdate.Description = serviceReceivingDTO.Description;
             serviceToUpdate.ImageUrl = serviceReceivingDTO.ImageUrl;
@@ -105,12 +114,142 @@ namespace HaloBiz.MyServices.Impl
             {
                 return new ApiResponse(500);
             }
+
+            summary += $"Details after change, \n {serviceToUpdate.ToString()} \n";
+
+            ModificationHistory history = new ModificationHistory()
+            {
+                ModelChanged = "Service",
+                ChangeSummary = summary,
+                ChangedById = context.GetLoggedInUserId(),
+                ModifiedModelId = updatedService.Id
+            };
+
+            await _modificationRepo.SaveHistory(history);
+            var serviceTransferDTO = _mapper.Map<ServicesTransferDTO>(updatedService);
+            return new ApiOkResponse(serviceTransferDTO);
+
+        }
+        public async Task<ApiResponse> ApproveService(HttpContext context, long id)
+        {
+            var serviceToUpdate = await _servicesRepository.FindServicesById(id);
+            if (serviceToUpdate == null)
+            {
+                return new ApiResponse(404);
+            }
+            
+            serviceToUpdate.PublishedApprovedStatus = true;
+            serviceToUpdate.IsRequestedForPublish = true;
+           
+            var updatedService = await _servicesRepository.UpdateServices(serviceToUpdate);
+
+            if (updatedService == null)
+            {
+                return new ApiResponse(500);
+            }
+            ModificationHistory history = new ModificationHistory()
+            {
+                ModelChanged = "Service",
+                ChangeSummary = $"Service with ServiceId: {updatedService.Id} was approved by user with userid {context.GetLoggedInUserId()}",
+                ChangedById = context.GetLoggedInUserId(),
+                ModifiedModelId = updatedService.Id
+            };
+
+            await _modificationRepo.SaveHistory(history);
+
             var serviceTransferDTO = _mapper.Map<ServicesTransferDTO>(updatedService);
             return new ApiOkResponse(serviceTransferDTO);
 
         }
 
-        public async Task<ApiResponse> DeleteService(long id)
+        public async Task<ApiResponse> RequestPublishService(HttpContext context, long id)
+        {
+            var serviceToUpdate = await _servicesRepository.FindServicesById(id);
+            if (serviceToUpdate == null)
+            {
+                return new ApiResponse(404);
+            }
+
+            serviceToUpdate.IsRequestedForPublish = true;
+           
+            var updatedService = await _servicesRepository.UpdateServices(serviceToUpdate);
+
+            if (updatedService == null)
+            {
+                return new ApiResponse(500);
+            }
+
+            ModificationHistory history = new ModificationHistory()
+            {
+                ModelChanged = "Service",
+                ChangeSummary = $"User with userId {context.GetLoggedInUserId()} requested for Service with ServiceId: {updatedService.Id} to be published ",
+                ChangedById = context.GetLoggedInUserId(),
+                ModifiedModelId = updatedService.Id
+            };
+
+            await _modificationRepo.SaveHistory(history);
+
+            var serviceTransferDTO = _mapper.Map<ServicesTransferDTO>(updatedService);
+            return new ApiOkResponse(serviceTransferDTO);
+
+        }
+        public async Task<ApiResponse> DisapproveService(HttpContext context, long id)
+        {
+            var serviceToUpdate = await _servicesRepository.FindServicesById(id);
+            if (serviceToUpdate == null)
+            {
+                return new ApiResponse(404);
+            }
+            
+            serviceToUpdate.PublishedApprovedStatus = false;
+            serviceToUpdate.IsRequestedForPublish = true;
+           
+            var updatedService = await _servicesRepository.UpdateServices(serviceToUpdate);
+
+            if (updatedService == null)
+            {
+                return new ApiResponse(500);
+            }
+            ModificationHistory history = new ModificationHistory()
+            {
+                ModelChanged = "Service",
+                ChangeSummary = $"Service with ServiceId: {updatedService.Id} was disapproved by user with userid {context.GetLoggedInUserId()}",
+                ChangedById = context.GetLoggedInUserId(),
+                ModifiedModelId = updatedService.Id
+            };
+
+            await _modificationRepo.SaveHistory(history);
+
+            var serviceTransferDTO = _mapper.Map<ServicesTransferDTO>(updatedService);
+            return new ApiOkResponse(serviceTransferDTO);
+
+        }
+
+        public async Task<ApiResponse> DeleteService(HttpContext context, long id)
+        {
+            var serviceToDelete = await _servicesRepository.FindServicesById(id);
+            if (serviceToDelete == null)
+            {
+                return new ApiResponse(404);
+            }
+
+            if (!await _servicesRepository.DeleteService(serviceToDelete))
+            {
+                return new ApiResponse(500);
+            }
+
+            DeleteLog deleteLog = new DeleteLog(){
+                DeletedById = context.GetLoggedInUserId(),
+                DeletedModelId = serviceToDelete.Id,
+                ChangeSummary = $"Deleted service with Id: {serviceToDelete.Id}",
+
+            };
+
+            await _deleteLogRepo.SaveDeleteLog(deleteLog);
+
+            return new ApiOkResponse(true);
+        }
+        public async Task<ApiResponse> DeleteService( long id)
         {
             var serviceToDelete = await _servicesRepository.FindServicesById(id);
             if (serviceToDelete == null)
