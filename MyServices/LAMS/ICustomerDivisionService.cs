b9bf9992517c55b2d08708fad0189901362e9d4e
Umoren Joshua <umorenjoshua50@gmail.com>
2021-01-25 18:20:55 +0100

    Added Interface for CustomerDivision

---
+++ MyServices/LAMS/ICustomerDivisionService.cs
@@ -0,0 +1,21 @@
+﻿using HaloBiz.DTOs.ApiDTOs;
+using HaloBiz.DTOs.ReceivingDTOs;
+using HaloBiz.DTOs.ReceivingDTOs.LAMS;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HaloBiz.MyServices.LAMS
+{
+    public interface ICustomerDivisionService
+    {
+        Task<ApiResponse> AddCustomerDivision(HttpContext context, CustomerDivisionReceivingDTO CustomerDivisionReceivingDTO);
+        Task<ApiResponse> GetAllCustomerDivisions();
+        Task<ApiResponse> GetCustomerDivisionById(long id);
+        Task<ApiResponse> GetCustomerDivisionByName(string name);
+        Task<ApiResponse> UpdateCustomerDivision(HttpContext context, long id, CustomerDivisionReceivingDTO CustomerDivisionReceivingDTO);
+        Task<ApiResponse> DeleteCustomerDivision(long id);
+    }
+}
