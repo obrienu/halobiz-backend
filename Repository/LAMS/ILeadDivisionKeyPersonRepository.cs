ca57683adda51daf2d727f19c52897f94835eb58
Umoren Joshua <umorenjoshua50@gmail.com>
2021-01-26 17:16:47 +0100

    Adds repo

---
+++ Repository/LAMS/ILeadDivisionKeyPersonRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using HaloBiz.Model.LAMS;
+
+namespace HaloBiz.Repository.LAMS
+{
+    public interface ILeadDivisionKeyPersonRepository
+    {
+        Task<LeadDivisionKeyPerson> SaveLeadDivisionKeyPerson(LeadDivisionKeyPerson entity);
+
+        Task<LeadDivisionKeyPerson> FindLeadDivisionKeyPersonById(long Id);
+
+        Task<IEnumerable<LeadDivisionKeyPerson>> FindAllLeadDivisionKeyPerson();
+
+        Task<LeadDivisionKeyPerson> UpdateLeadDivisionKeyPerson(LeadDivisionKeyPerson entity);
+        Task<bool> DeleteLeadDivisionKeyPerson(LeadDivisionKeyPerson entity);
+    }
+}
