11695472dd95830f9ddef4ae1a0df727b95bcc12
obrienu <obrienules@gmail.com>
2021-01-28 11:21:08 +0100

    Updates the QuoteServcie and ContractService Fields

--- Model/LAMS/ContractService.cs
+++ Model/LAMS/ContractService.cs
@@ -10,31 +10,44 @@ namespace HaloBiz.Model.LAMS
     {
         [Key]
         public long Id { get; set; }
-        public DateTime? ActivationDate { get; set; } = null;
-        public GuardType? GuardType { get; set; } = null;
-        [StringLength(500)]
-        public string Location { get; set; }
-        public DateTime? EntryDateTime { get; set; } = null;
-        [StringLength(500)]
-        public string PickUp { get; set; }
-        [StringLength(500)]
-        public string DropOff { get; set; }
-        public DateTime? ServiceDateTime { get; set; } = null;
-        public DateTime? StartDate { get; set; }
-        public DateTime? EndDate { get; set; }
-        public long Quantity { get; set; } = 1;
+        public double? UnitPrice {get ; set; }
+        public long Quantity {get ; set; }
+        public double Discount {get ; set; }
+        public double? VAT {get ; set; }
+        public double? BillableAmount {get ; set; }
+        public double? Budget {get ; set; }
+        public DateTime? ContractStartDate {get ; set; }
+        public DateTime? ContractEndDate {get ; set; }
+        public TimeCycle? PaymentCycle {get ; set; }
+        public long? PaymentCycleInDays {get ; set; }
+        public DateTime? FirstInvoiceSendDate {get ; set; }
+        public TimeCycle? InvoicingInterval  {get ; set; }
+        [StringLength(2000)]
+        public string ProblemStatement {get ; set; }
+        public DateTime? ActivationDate {get ; set; }
+        public DateTime? FulfillmentStartDate {get ; set; }
+        public DateTime? FulfillmentEndDate {get ; set; }
+        public DateTime? TentativeDateForSiteSurvey {get ; set; }
+        public DateTime? PickupDateTime {get ; set; }
+        public DateTime? DropoffDateTime {get ; set; }
         [StringLength(1000)]
-        public string ProblemStatement { get; set; }
-        public bool IsGuardTouringRequired { get; set; } = false;
-        public double Discount { get; set; } = 0.0;
-        public double Price { get; set; }
-        public double VAT { get; set; } = 0.0;
-        public double BillableAmount  { get; set; } = 0.0;
-        public double Budget { get; set; } = 0.0;
-        public long? PaymentCycle { get; set; }
-        public int? InvoiceSendInterval { get; set; }
-        public DateTime? InvoiceSendDate { get; set; }
-        public bool IsConvertedToContractService { get; set; } =false;
+        public string PickupLocation {get ; set; }
+        [StringLength(1000)]
+        public string Dropofflocation {get ; set; }
+        [StringLength(200)]
+        public string BeneficiaryName {get ; set; }
+        [StringLength(200)]
+        public string BeneficiaryIdentificationType {get ; set; }
+        [StringLength(200)]
+        public string BenificiaryIdentificationNumber {get ; set; }
+
+        public DateTime? TentativeProofOfConceptStartDate {get ; set; }
+        public DateTime? TentativeProofOfConceptEndDate {get ; set; }
+        public DateTime? TentativeFulfillmentDate {get ; set; }
+        public DateTime? ProgramCommencementDate {get ; set; }
+        public long? ProgramDuration {get ; set; }
+        public DateTime? ProgramEndDate {get ; set; }
+        public DateTime? TentativeDateOfSiteVisit {get ; set; }
         public long ServiceId { get; set; }
         public Services Service { get; set; }
         public long QuoteId { get; set; }
