d994b4b3d3d614aa4b23727d18ec6b39abfecbaa
Umoren Joshua <umorenjoshua50@gmail.com>
2021-01-28 13:54:14 +0100

    Added DTO for Quote services

---
+++ DTOs/TransferDTOs/LAMS/QuoteTransferDTO.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+using HaloBiz.Helpers;
+using HaloBiz.Model;
+using HaloBiz.Model.LAMS;
+
+namespace HaloBiz.DTOs.TransferDTOs.LAMS
+{
+    public class QuoteTransferDTO
+    {
+        public long Id { get; set; }
+        public string ReferenceNo { get; set; }
+        public long LeadDivisionId { get; set; }
+        public LeadDivision LeadDivision { get; set; }
+        public bool IsConvertedToContract { get; set; }
+        public IEnumerable<QuoteService> QuoteServices { get; set; }
+        public VersionType Version { get; set; }
+        public long CreatedById { get; set; }
+        public virtual UserProfile CreatedBy { get; set; }
+        public IEnumerable<Contract> Contracts { get; set; }
+    }
+}
