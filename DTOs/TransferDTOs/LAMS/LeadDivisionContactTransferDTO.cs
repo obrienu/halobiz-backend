1bb44a016cc157a3e94060c6182d12dfd464a314
Umoren Joshua <umorenjoshua50@gmail.com>
2021-01-29 13:08:07 +0100

    added endpoint for lead division contact and tied it to a lead division.

--- DTOs/TransferDTOs/LAMS/LeadDivisionContactTransferDTO.cs
+++ DTOs/TransferDTOs/LAMS/LeadDivisionContactTransferDTO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HaloBiz.Model.LAMS;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,6 @@ namespace HaloBiz.DTOs.TransferDTOs.LAMS
 {
     public class LeadDivisionContactTransferDTO : ContactTransferDTO
     {
+        public ContactType Type { get; set; }
     }
 }
