6143638285b2c600f9d3210080c84b9bfd569017
Umoren Joshua <umorenjoshua50@gmail.com>
2021-01-31 11:07:37 +0100

    Adds NegotiationDocument Model in Lams

---
+++ Model/LAMS/NegotiationDocument.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HaloBiz.Model.LAMS 
+{
+    public class NegotiationDocument : Documents
+    {
+        public long QuoteServiceId { get; set; }
+        public QuoteService QuoteService { get; set; }
+    }
+}
